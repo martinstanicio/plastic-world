7898176fa06a5b3cecffa9ebf7dd7c2a31fe639b
Martín Stanicio <martuytimo@gmail.com>
2024-04-09 14:48:48 -0300

    replace hero image with logo and only show it in big screens

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,35 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import hero from "@/assets/hero.png";
+import logo from "@/assets/logo.png";
 
 import { Button } from "./ui/button";
 
 export default function Hero() {
   return (
     <section className="mx-auto grid max-w-6xl md:grid-cols-2">
-      <div className="relative grid place-items-center sm:pb-8 sm:pt-16 md:order-last md:px-4 md:py-16">
-        <svg
-          viewBox="0 0 100 63"
-          className="absolute bottom-0 right-0 -z-10 h-full max-lg:hidden"
-        >
-          <path
-            d="M99 3C99 3 82.5 3 80 13.5C75.9676 26.4784 86.5 41.5 70.5 54C55.7753 65.5037 41.5 38 20.5 49C11.9123 53.4983 10 63 10 63"
-            className="fill-none stroke-primary stroke-2"
-            strokeDasharray="5"
-            strokeLinecap="round"
-          />
-        </svg>
-        <Image
-          src={hero}
-          alt="Foto del frente de nuestro local en el puerto"
-          priority
-          placeholder="blur"
-          sizes="(min-width: 1100px) 480px, (min-width: 780px) calc(40.67vw + 41px), (min-width: 640px) 480px, 100vw"
-          className="aspect-[4/3] w-full bg-secondary object-cover object-center max-sm:max-h-64 sm:max-w-[30rem] sm:rounded-md sm:shadow-lg"
-        />
-      </div>
-      <div className="prose z-10 flex max-w-md flex-col justify-center px-4 pb-16 pt-8 prose-a:no-underline md:pr-0 md:pt-16">
+      <div className="prose z-10 flex max-w-md flex-col justify-center px-4 py-8 prose-a:no-underline md:px-8 md:py-16 md:pr-0">
         <h1 className="text-balance">
           Encontrá lo que necesitás con{" "}
           <span className="text-primary">Plastic World</span>
@@ -51,6 +30,26 @@ export default function Hero() {
             <Link href="/contacto">Contacto</Link>
           </Button>
         </div>
+      </div>
+      <div className="relative grid place-items-center px-4 py-16 max-md:hidden">
+        <svg
+          viewBox="0 0 100 63"
+          className="absolute bottom-0 right-0 -z-10 h-full max-lg:hidden"
+        >
+          <path
+            d="M99 3C99 3 82.5 3 80 13.5C75.9676 26.4784 86.5 41.5 70.5 54C55.7753 65.5037 41.5 38 20.5 49C11.9123 53.4983 10 63 10 63"
+            className="fill-none stroke-primary stroke-2"
+            strokeDasharray="5"
+            strokeLinecap="round"
+          />
+        </svg>
+        <Image
+          src={logo}
+          alt="Logo de Plastic World"
+          priority
+          sizes="auto"
+          className="aspect-[4/3] w-full max-w-[30rem] object-contain object-center drop-shadow-2xl"
+        />
       </div>
     </section>
   );
